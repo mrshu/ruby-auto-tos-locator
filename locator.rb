5bb4ecd68143d88aef67d8a8d4739cd0730daf35
mr.Shu <mr@shu.io>
2014-10-12 21:15:50 +0200

    update: Added comments, improved heuristics
    * Added comments to clarify the rationale behind the code
    * Corrected output where necessary
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- locator.rb
+++ locator.rb
@@ -14,12 +14,16 @@ def article_xpath(elem)
   clean_tags = 0
   out = ''
   for _ in 1..3
-    if elem.attr('class') =~ /privacy|policy|service/i
+    # if there is a class that explicitely as terms, privacy, policy or service
+    # in it then it should be enough to locate our desired content on the page
+    # by using this element
+    if elem.attr('class') =~ /terms|privacy|policy|service/i
       out = stringify_node(elem, 'class') + out
       break
     end
 
-    if elem.attr('id') =~ /privacy|policy|service/i
+    # the same logic as with class applies
+    if elem.attr('id') =~ /terms|privacy|policy|service/i
       out = stringify_node(elem, 'id') + out
       break
     end
@@ -30,15 +34,18 @@ def article_xpath(elem)
       out = stringify_node(elem, 'class') + out
     else
       clean_tags += 1
+      out = '/' + elem.name + out
     end
 
     elem = elem.parent
   end
 
+  # if we ran into just clean tags rather return a direct XPath than our 3-way
+  # approximation
   if clean_tags == 3
     return Nokogiri::CSS.xpath_for elem.css_path
   else
-    return out
+    return '/' + out
   end
 
 end
